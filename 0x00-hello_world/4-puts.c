5987a432b58d097f09b559d97707c2d1931a9523
joshua-983 <officialgh983@gmail.com>
2023-06-16 13:17:28 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
- * main A program that prints a whole line with print function
- * Return: 0 (Success);
+ * main - A program that prints a line with puts function
+ * Return: 0 (Success)
  */
 int main(void)
 {
