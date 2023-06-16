d382fc2d625bebc55ff88b5bab43df5003d05f9b
joshua-983 <officialgh983@gmail.com>
2023-06-16 11:05:25 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main A program that prints a whole line with print function
+ * Return: 0 (successfully Passed)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
