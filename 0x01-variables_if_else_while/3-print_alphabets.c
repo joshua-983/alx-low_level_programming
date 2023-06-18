6d045e0fae83b3d8c59355652407dcb491669b9b
joshua-983 <officialgh983@gmail.com>
2023-06-18 11:39:53 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,17 +7,16 @@
  * Return: Always 0 (Success)
  */
 
-int main(void)
-
+int mian(void)
 {
-	char low;
+	int n;
 
-	for (low = 'a'; low <= 'z'; low++)
-	putchar(low);
-
-	for (low = 'A'; low <= 'Z'; low++)
-	putchar(low)
-	putchar('\n');
-
-	return(0);
-}
+	srand(time (0);
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+		printf ("%d is positive\n", n);
+	else if (n == 0)
+		ptintf ("%d is zero\n", n);
+	else 	
+		printf("%d is negative\n", n);
+	return (0);
