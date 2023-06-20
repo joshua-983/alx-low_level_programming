d6c83d6c6676516bb0bc3df888108e31d2718cb5
joshua-983 <officialgh983@gmail.com>
2023-06-19 20:25:08 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,13 @@
-#include <stdio.h>
 #include <unistd.h>
 /**
- * main - a program that prints a line
- * Return: 1 (Succesis)
+ * main - prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
+ * followed by a newline, to standard error.
+ * Return: Always 1.
  */
 int main(void)
+
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n," 59);
+	write(2, "and that piece of art is useful\" -Dora Korpar, 2015-10-19\n", 59);
 return (1);
+
 }
