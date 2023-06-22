f54887aa54afd6fd3f5eee08a5f376f90c1e657f
joshua-983 <officialgh983@gmail.com>
2023-06-21 18:14:07 -0700

    ./8-24 | head

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,18 +1,20 @@
 #include "main.h"
 
 /**
- * jack_baurer - Prints evry minute of the day of Jack Bauer
+ * jack_bauer - Prints evry minute of the day of Jack Bauer
  * Return ...
- * void jack_bauer(void)
  */
 
+void jack_bauer(void)
+ 
 {
 	int a, b, c, d;
-	for (a = 0; a <= 2; a+++)
+
+	for (a = 0; a <= 2; a++)
 	{
 	for (b = 0; b <= 9; b++)
 	{
-	if ()((a<= 1 && b <= 9) || (a <= 2 && b <= ))
+	if ((a<= 1 && b <= 9) || (a <= 2 && b <= 3 ))
 	{
 	for (c = 0; c <= 5; c++)
 	{
