26baf2b4e8ab7d4db23e0b789d67431b6af3ae3d
joshua-983 <officialgh983@gmail.com>
2023-06-30 09:56:40 -0700

    2-strlen.c

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -10,7 +10,8 @@
 
 void swap_int(int *a, int *b)
 
-/* the function that swaps the values of two integers. */
+/* the function that swaps the values of two integers
+
 {
 	int temp = *a;
 	*a = *b;
