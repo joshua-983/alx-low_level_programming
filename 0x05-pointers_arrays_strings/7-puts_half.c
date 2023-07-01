dde0bae222ed317d2a91b09919aba09d922b74b0
joshua-983 <officialgh983@gmail.com>
2023-06-30 20:43:49 -0700

    7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -20,7 +20,7 @@ void puts_half(char *str)
 	start_index = (length - 1) / 2;
 	}
 
-	for (int i = start_index; i < length; i++)
+	for ( i = start_index; i < length; i++)
 	{
         putchar(str[i]);
 	}
