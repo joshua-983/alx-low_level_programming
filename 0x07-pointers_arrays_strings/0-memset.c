37484cc6ff954ff8461c558a2c7bff00e86fc00b
joshua-983 <officialgh983@gmail.com>
2023-07-03 14:52:19 -0700

    0-memset.c

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _memcpy - entry point
+ * @dest: inpt
+ * @src: input
+ * @n input
+ * Return: Always 0 (success)
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	dest[i] = src[i];
+
+	return (dest);
+
+}
