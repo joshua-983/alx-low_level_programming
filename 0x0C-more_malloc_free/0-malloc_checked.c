c33a535be5a0b71e41b8f3f155920e5e0115f741
joshua-983 <officialgh983@gmail.com>
2023-07-13 12:56:26 -0700

    commit

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * *malloc_checked - allocate memory
+ * @b:number of bytes
+ * Return: a pointer.
+ */
+
+void *malloc_checked(unsigned int b)
+
+{
+	void *ptr = malloc(b);
+
+	if (ptr == NULL) 
+	{
+	printf("Error: Memory allocation failed.\n");
+	exit(98);
+	}
+	return (ptr);
+}
+
