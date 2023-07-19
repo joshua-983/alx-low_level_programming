3af7b6aa670ca585f7fbc47f7c574eeafc1b9888
joshua-983 <officialgh983@gmail.com>
2023-07-19 15:57:56 -0700

    put

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,24 +1,29 @@
-#include <stdio.h>
-
+#include "main.h"
 /**
- * puts2 - a function that prints every other character as string.
- * @str: the sring to worked on.
- * Return: void
+ * puts2 - function should print only one character out of two
+ * starting with the first one
+ * @str: input
+ * Return: print
  */
-
 void puts2(char *str)
 {
-	int i = 0;
-	int j = 0;
+	int longi = 0;
+	int t = 0;
+	char *y = str;
+	int o;
 
-	while (str[i] != '\0')
+	while (*y != '\0')
 	{
-	i++;
+		y++;
+		longi++;
 	}
-	for (i = 0; i < j; i += 2)
-
-	putchar(str[i]);
-	i += 2;
+	t = longi - 1;
+	for (o = 0 ; o <= t ; o++)
+	{
+		if (o % 2 == 0)
+	{
+		_putchar(str[o]);
 	}
-	putchar('\n');
+	}
+	_putchar('\n');
 }
