bbf989f6e2d8cd050def05ba9804e84e1a98e878
joshua-983 <officialgh983@gmail.com>
2023-08-01 04:37:35 -0700

    alx-linked list

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,19 +1,19 @@
 #include "lists.h"
 
 /**
- * list_len - return link list numbers
- * @h: link list type
- * Return: nodes
+ * listint_len - function that returns the number of elements in a linked listint_t list
+ * @h: we have to linked list listint_t to traverse the code.
+ * Return: linked list numbers of node.
  */
 
 size_t listint_len(const listint_t *h)
 
 {
 	size_t num = 0;
-	while (h)
-	{
-	num++;
-	h = h->next;
-	}
+		while (h)
+		{
+			num++;
+			h = h->next;
+		}
 	return (num);
 }
