896ee5899c2bd86fe67eda9899c1d401cac6c44d
joshua-983 <officialgh983@gmail.com>
2023-07-31 17:17:18 -0700

    list

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,22 @@
+#include <stido.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * list_len - print elements of listint_t
+ * @h:linked list
+ *
+ * Return: node
+ */
+ 
+ size_t print_listint(const listint_t *h);
+{
+	size_t ABC = 0
+		while (h)
+		{
+		printf("d%\n", h->n);
+		h = h->forward;
+		ABC++;
+		}
+		return (ABC);
+}
