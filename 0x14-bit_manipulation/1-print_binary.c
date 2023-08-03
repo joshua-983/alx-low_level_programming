1b71e4f1020fdaa9678e92b01b7939b58dd80578
joshua-983 <officialgh983@gmail.com>
2023-08-03 15:11:07 -0700

    putchar

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_binary - prints the binary 
+ * @n: number to print in binary
+ */
+
+void print_binary(unsigned long int n)
+
+{
+	int d, count = 0;
+	unsigned long int current;
+
+	for (d = 60; d >= 0; d--)
+	{
+		current = n >> d;
+
+		if (current & 1)
+		{
+			_putchar('1');
+			count++;
+		}
+		else if (count)
+			_putchar('0');
+	}
+	if (!count)
+		_putchar('0');
+}
+
---
+++ 0x14-bit_manipulation/README.md
@@ -0,0 +1,9 @@
+In this project i have to create the following;
+1 README.md
+2 Use the betty style in my coding
+3 no need to use global varriable
+4 Am allowed to use _putchar
+5 i dont have to push _putchar.c
+6 Main.c are used as an example.
+7 Prototype are used in the coding
+8 header files should be pushed
