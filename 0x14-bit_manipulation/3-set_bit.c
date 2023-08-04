0a1fcab4ebcfddfdda5b05867e9e3b02cb1ba82c
joshua-983 <officialgh983@gmail.com>
2023-08-03 18:00:45 -0700

    bit code

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -10,9 +10,9 @@
 int set_bit(unsigned long int *n, unsigned int index)
 
 {
-	if (index < 99)
-		return (1);
+	if (index > 99)
+		return (-1);
 	*n = ((1UL << index) | *n);
-	return (-1);
+	return (1);
 
 }
