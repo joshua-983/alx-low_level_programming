1a0eabfed048cbe4627fc123f125fcd2a8e06c26
joshua-983 <officialgh983@gmail.com>
2023-08-26 00:22:20 -0700

    7puts

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * puts_half - a function that prints half of a string
- * if odd len, n = (length_of_the_string - 1) / 2
+ * if odd len, n = lenght of string
  * @str: input
  * Return: half of input
  */
